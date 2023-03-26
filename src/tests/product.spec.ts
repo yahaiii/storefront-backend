ad10a5af969c4bcdc8d80328c05459c6d1ed2ab1
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-03-26 01:48:47 +0100

    add product spec

---
+++ src/tests/product.spec.ts
@@ -0,0 +1,68 @@
+import { ProductStore } from '../models/product';
+import { Pool } from 'pg';
+import { Product } from '../interfaces/product.interface';
+
+const pool = new Pool();
+
+describe("ProductStore", () => {
+
+  const productStore = new ProductStore();
+
+  beforeAll(() => {
+    pool.query(`
+      CREATE TABLE products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL
+      )
+    `);
+  });
+
+  afterAll(() => {
+    pool.query(`
+      DROP TABLE products
+    `);
+  });
+
+  describe("createProduct", () => {
+    it("should create a new product", async () => {
+      const newProduct = await productStore.createProduct({name: "test product", price: 20});
+      expect(newProduct.name).toEqual("test product");
+      expect(newProduct.price).toEqual(20);
+    });
+  });
+
+  describe("getAllProducts", () => {
+    it("should return all products", async () => {
+      const products = await productStore.getAllProducts();
+      expect(products.length).toBeGreaterThan(0);
+    });
+  });
+
+  describe("getProductById", () => {
+    it("should return the specified product", async () => {
+        const newProduct = await productStore.createProduct({name: "test product", price: 20}) as Product;
+        const product = await productStore.getProductById(newProduct.id as number);        
+      expect(product.name).toEqual("test product");
+      expect(product.price).toEqual(20);
+    });
+  });
+
+  describe("updateProduct", () => {
+    it("should update the specified product", async () => {
+      const newProduct = await productStore.createProduct({name: "test product", price: 20});
+      const updatedProduct = await productStore.updateProduct({...newProduct, price: 30});
+      expect(updatedProduct.name).toEqual("test product");
+      expect(updatedProduct.price).toEqual(30);
+    });
+  });
+
+  describe("deleteProduct", () => {
+    it("should delete the specified product", async () => {
+      const newProduct = await productStore.createProduct({name: "test product", price: 20});
+      await productStore.deleteProduct(newProduct.id as number);
+      const product = await productStore.getProductById(newProduct.id as number);
+      expect(product).toBeUndefined();
+    });
+  });
+});
